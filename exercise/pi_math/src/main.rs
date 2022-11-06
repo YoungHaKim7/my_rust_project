34ac42b76668f0daacde42a78f2ad4d50b0d9376
YoungHa Kim <ytok1108@aol.com>
2022-11-06 18:13:01 +0900

    rust pi__math

--- exercise/pi_math/src/main.rs
+++ exercise/pi_math/src/main.rs
@@ -1,8 +1,14 @@
-// https://crates.io/crates/num_cpus
 // https://rust-lang-nursery.github.io/rust-cookbook/hardware/processor.html
 
-extern crate num_cpus;
+use num::complex::Complex;
+use std::f64::consts::PI;
 
 fn main() {
-    println!("Number of logical cores is {}", num_cpus::get());
+    let x = Complex::new(0.0, 2.0 * PI);
+
+    println!("e^(2i * pi) = {}", x.exp()); // =~1
 }
+
+// e^(1i * pi) = -1+0.00000000000000012246467991473532i
+// e^(2i * pi) = -1-0.00000000000000024492935982947064i
+// e^(3i * pi) = -1+0.00000000000000036739403974420594i
