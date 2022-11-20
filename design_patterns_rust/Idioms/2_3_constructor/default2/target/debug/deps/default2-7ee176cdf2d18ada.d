cb9c6cac724eedcfaaf7f0fa9648728c9bb4bd97
YoungHa Kim <ytok1108@aol.com>
2022-11-20 20:05:58 +0900

    default2

--- design_patterns_rust/Idioms/2_3_constructor/default2/target/debug/deps/default2-7ee176cdf2d18ada.d
+++
@@ -1,5 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/my_rust_project/design_patterns_rust/Idioms/2_3_constructor/default2/target/debug/deps/default2-7ee176cdf2d18ada.rmeta: src/main.rs
-
-/Users/globalyoung/Documents/Project/Github/rust_project/my_rust_project/design_patterns_rust/Idioms/2_3_constructor/default2/target/debug/deps/default2-7ee176cdf2d18ada.d: src/main.rs
-
-src/main.rs:
