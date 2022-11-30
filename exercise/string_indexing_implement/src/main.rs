fa4b1796e67fbaae60471de4c02ab5b61f598321
unknown <ytok1108@aol.com>
2022-11-30 16:36:26 +0900

    string indexing implement __result: H e l l o .
    안
    녕
    하
    세
    요

---
+++ exercise/string_indexing_implement/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let some_str = String::from("Hello. \n 안녕하세요");
+    let some_vec = some_str.chars().collect::<Vec<char>>();
+    for i in 0..some_vec.len() {
+        println!("{}", some_vec[i]);
+    }
+}
