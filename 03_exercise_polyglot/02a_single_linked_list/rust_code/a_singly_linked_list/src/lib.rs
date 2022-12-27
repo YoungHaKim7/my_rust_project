982fc5cb82cf5560d0f618e3746779d947e71296
YoungHa Kim <ytok1108@aol.com>
2022-12-28 00:01:02 +0900

    A singly linked list __final code2

--- 03_exercise_polyglot/02a_single_linked_list/rust_code/a_singly_linked_list/src/lib.rs
+++ 03_exercise_polyglot/02a_single_linked_list/rust_code/a_singly_linked_list/src/lib.rs
@@ -3,8 +3,8 @@ pub struct LinkedList<T> {
 }
 
 impl<T> LinkedList<T> {
-    fn empty() -> LinkedList<T> {
-        LinkedList { head: None }
+    fn empty() -> Self {
+        Self { head: None }
     }
 
     fn push(&mut self, element: T) {
