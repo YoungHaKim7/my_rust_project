6c2799ce965862a917c6558824f7c67bbe838f9b
YoungHa Kim <ytok1108@aol.com>
2022-12-31 23:31:23 +0900

    Rust Optimization

--- 09_Statistics_benchmarking_in_Rust/criterion_rs/README.md
+++ 09_Statistics_benchmarking_in_Rust/criterion_rs/README.md
@@ -13,6 +13,28 @@ fib 20                  time:   [19.714 µs 19.724 µs 19.735 µs]
 
 ```
 
+# cargo criterion
+
+```
+$ cargo criterion
+
+   Compiling criterion_rs v0.1.0 (/Users/globalyoung/Documents/test/test/rust/my_rust_project/09_Statistics_benchmarking_in_Rust/criterion_rs)
+    Finished bench [optimized] target(s) in 1.11s
+    
+fib 20                  time:   [19.715 µs 19.725 µs 19.736 µs]
+                        change: [-0.0848% +0.0979% +0.2891%] (p = 0.31 > 0.05)
+                        No change in performance detected.
+
+Fibonacci/Recursive     time:   [311.25 ps 312.00 ps 312.90 ps]
+Fibonacci/Iterative     time:   [6.8448 ns 6.8478 ns 6.8510 ns]
+
+Fibonacci3/Recursive/20 time:   [311.34 ps 311.89 ps 312.57 ps]
+Fibonacci3/Iterative/20 time:   [6.8395 ns 6.8427 ns 6.8463 ns]
+Fibonacci3/Recursive/21 time:   [311.08 ps 311.25 ps 311.46 ps]
+Fibonacci3/Iterative/21 time:   [7.1507 ns 7.1542 ns 7.1584 ns]
+
+```
+
 <hr>
 
 <br>
--- 09_Statistics_benchmarking_in_Rust/criterion_rs/benches/my_benchmark.rs
+++ 09_Statistics_benchmarking_in_Rust/criterion_rs/benches/my_benchmark.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 fn fibonacci(n: u64) -> u64 {
     match n {
@@ -12,5 +12,53 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn fibonacci_slow(n: u64) -> u64 {
+    match n {
+        0 | 1 => 1,
+        n => fibonacci_slow(n - 1) + fibonacci_slow(n - 2),
+    }
+}
+
+fn fibonacci_fast(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
+    }
+}
+
+fn compare_fibonaccis(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Fibonacci");
+
+    group.bench_with_input("Recursive", &20, |b, i| b.iter(|| fibonacci_slow(*i)));
+    group.bench_with_input("Iterative", &20, |b, i| b.iter(|| fibonacci_fast(*i)));
+}
+fn compare_fibonaccis_group(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Fibonacci3");
+    for i in 20..=21 {
+        group.bench_with_input(BenchmarkId::new("Recursive", i), &i, |b, i| {
+            b.iter(|| fibonacci_slow(*i))
+        });
+        group.bench_with_input(BenchmarkId::new("Iterative", i), &i, |b, i| {
+            b.iter(|| fibonacci_fast(*i))
+        });
+    }
+    group.finish()
+}
+
+criterion_group!(
+    benches,
+    criterion_benchmark,
+    compare_fibonaccis,
+    compare_fibonaccis_group
+);
 criterion_main!(benches);
