c74c4658dfa20bf7d8a9b053e63bc962368990bd
YoungHa Kim <ytok1108@aol.com>
2023-01-22 22:28:26 +0900

    Simple GUI basic

---
+++ 04_GUI_Rust/01_GUI_basic/smallgui/src/main.rs
@@ -0,0 +1,24 @@
+// A Simple GUI Libray
+
+pub trait Widget {
+    fn width(&self) -> usize;
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write);
+}
+
+struct Window {
+    title: String,
+    widgets: Vec<Box<dyn Widget>>,
+}
+
+impl Window {
+    fn new(title: &str) -> Window {
+        Window {
+            title: title.to_owned(),
+            widgets: Vec::new(),
+        }
+    }
+}
+fn main() {
+    let mut window = Window::new("Rust GUI Demo 1.23");
+}
