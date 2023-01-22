ae5f2660729d8ff298b1e0d93bf4b42d63ea8bb5
YoungHa Kim <ytok1108@aol.com>
2023-01-22 22:53:06 +0900

    Simple GUI basic2

--- 04_GUI_Rust/01_GUI_basic/smallgui/src/main.rs
+++ 04_GUI_Rust/01_GUI_basic/smallgui/src/main.rs
@@ -4,11 +4,21 @@ pub trait Widget {
     fn width(&self) -> usize;
 
     fn draw_into(&self, buffer: &mut dyn std::fmt::Write);
+
+    fn draw(&self) {
+        let mut buffer = String::new();
+        self.draw_into(&mut buffer);
+        println!("{}", &buffer);
+    }
 }
 
 struct Window {
     title: String,
     widgets: Vec<Box<dyn Widget>>,
+}
+
+struct Label {
+    label: String,
 }
 
 impl Window {
@@ -18,7 +28,61 @@ impl Window {
             widgets: Vec::new(),
         }
     }
+
+    fn add_widget(&mut self, widget: Box<dyn Widget>) {
+        self.widgets.push(widget);
+    }
 }
+
+impl Widget for Window {
+    fn width(&self) -> usize {
+        std::cmp::max(
+            self.title.chars().count(),
+            self.widgets.iter().map(|w| w.width()).max().unwrap_or(0),
+        )
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        let mut inner = String::new();
+        for widget in &self.widgets {
+            widget.draw_into(&mut inner);
+        }
+
+        let window_width = self.width();
+
+        writeln!(buffer, "+={:-<window_width$}=+", "").unwrap();
+        writeln!(buffer, "| {:^window_width$} |", &self.title).unwrap();
+        writeln!(buffer, "+={:=<window_width$} |", "").unwrap();
+        for line in inner.lines() {
+            writeln!(buffer, "| {:window_width$} |", line).unwrap();
+        }
+        writeln!(buffer, "+-{:<window_width$}-+", "").unwrap();
+    }
+}
+
+impl Label {
+    fn new(label: &str) -> Label {
+        Label {
+            label: label.to_owned(),
+        }
+    }
+}
+
+impl Widget for Label {
+    fn width(&self) -> usize {
+        self.label
+            .lines()
+            .map(|line| line.chars().count())
+            .max()
+            .unwrap_or(0)
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        writeln!(buffer, "{}", &self.label).unwrap();
+    }
+}
+
 fn main() {
     let mut window = Window::new("Rust GUI Demo 1.23");
+    window.add_widget(Box::new(Label::new("This is a small text GUI Demo.")));
 }
