654c5562876ee039396e8a45f395c9029f536904
YoungHa Kim <ytok1108@aol.com>
2023-02-15 23:59:06 +0900

    coordinate_refactoring

---
+++ 01_Rust_Tutorial_Full_course/04_common_newbie_mistakes_in_Rust/03_etc_newbie/coordinate_refactoring2/target/debug/deps/coordinate_refactoring2-1bfbd60d4e53e986.d
@@ -0,0 +1,5 @@
+/Users/globalyoung/Documents/test/test/test/my_rust_project/01_Rust_Tutorial_Full_course/04_common_newbie_mistakes_in_Rust/03_etc_newbie/coordinate_refactoring2/target/debug/deps/coordinate_refactoring2-1bfbd60d4e53e986: src/main.rs
+
+/Users/globalyoung/Documents/test/test/test/my_rust_project/01_Rust_Tutorial_Full_course/04_common_newbie_mistakes_in_Rust/03_etc_newbie/coordinate_refactoring2/target/debug/deps/coordinate_refactoring2-1bfbd60d4e53e986.d: src/main.rs
+
+src/main.rs:
