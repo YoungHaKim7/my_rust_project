fc1cd3847f5cc0b4d5c45be0c31d922740f6390f
unknown <ytok1108@aol.com>
2023-04-05 14:45:34 +0900

    Struct & enum & impl & default

---
+++ 02_exercise_Rust_code/struct_enum_impl_default/README.md
@@ -0,0 +1,5 @@
+# Result
+
+```
+Character { name: "Billy", age: 15, height: 170, weight: 70, lifestate: Alive }
+```
---
+++ 02_exercise_Rust_code/struct_enum_impl_default/src/main.rs
@@ -0,0 +1,49 @@
+#[derive(Debug)]
+struct Character {
+    name: String,
+    age: u8,
+    height: u32,
+    weight: u32,
+    lifestate: LifeState,
+}
+
+#[derive(Debug)]
+enum LifeState {
+    Alive,
+    Dead,
+    NeverAlive,
+    Uncertain,
+}
+
+impl Character {
+    fn new(name: String, age: u8, height: u32, weight: u32, alive: bool) -> Self {
+        Self {
+            name,
+            age,
+            height,
+            weight,
+            lifestate: if alive {
+                LifeState::Alive
+            } else {
+                LifeState::Dead
+            },
+        }
+    }
+}
+
+impl Default for Character {
+    fn default() -> Self {
+        Self {
+            name: "Billy".to_string(),
+            age: 15,
+            height: 170,
+            weight: 70,
+            lifestate: LifeState::Alive,
+        }
+    }
+}
+
+fn main() {
+    let character_1 = Character::default();
+    println!("{:?}", character_1);
+}
